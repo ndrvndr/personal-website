7108a119b1a54552a45b937bddfaeb08b2a0c812
ndrvndr <andreavindra37@gmail.com>
2023-10-14 12:53:51 +0700

    feat: add top loader using next top loader

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import NextTopLoader from "nextjs-toploader";
 import Bottombar from "./Bottombar";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
@@ -6,13 +7,18 @@ import Sidebar from "./Sidebar";
 export default function Layouts({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <div
-        className={clsx(
-          "sticky top-0 z-10",
-          "h-2 w-full",
-          "bg-gradient-linear",
-        )}
+      <NextTopLoader
+        color="#B3FFAB"
+        initialPosition={0.08}
+        crawlSpeed={200}
+        height={3}
+        crawl={true}
+        showSpinner={true}
+        easing="ease"
+        speed={200}
+        shadow="0 0 10px #B3FFAB,0 0 5px #12FFF7"
       />
+
       <div
         className={clsx(
           "md:flex",
