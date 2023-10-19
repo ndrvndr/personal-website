2a24c60e0d3c257b272759e5e7021ce0ad2f0ef6
ndrvndr <andreavindra37@gmail.com>
2023-10-19 16:01:35 +0700

    fix:fixed build failure by not using the InputForm component in the Blog page

--- src/app/(root)/blog/Search.tsx
+++ src/app/(root)/blog/Search.tsx
@@ -1,7 +1,7 @@
 "use client";
-import InputForm from "@/components/elements/Input";
 import Tag from "@/components/elements/Tag";
 import { TAGS } from "@/constants/blog";
+import clsx from "clsx";
 import { motion } from "framer-motion";
 import { useState } from "react";
 
@@ -23,12 +23,23 @@ export default function Search() {
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.2 }}
       >
-        <InputForm
+        <label htmlFor="search" className="primary text-sm md:text-base">
+          Search
+        </label>
+        <input
           id="search"
-          label="Search"
-          placeholder="Search..."
           type="search"
+          placeholder="Search..."
           value={selectedTags}
+          className={clsx(
+            "border__color",
+            "secondary text-sm",
+            "mt-2 w-full px-2 py-3",
+            "rounded-md bg-transparent",
+            "placeholder:text-sm placeholder:text-neutral-500",
+            "focus:outline-double focus:outline-neutral-500",
+            "md:text-base md:placeholder:text-base",
+          )}
         />
       </motion.div>
 
@@ -36,7 +47,7 @@ export default function Search() {
         initial={{ opacity: 0, y: 50 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.4 }}
-        className="mb-8 flex flex-wrap justify-start gap-2 text-sm"
+        className="mb-8 mt-2 flex flex-wrap justify-start gap-2 text-sm"
       >
         <span className="primary text-sm md:text-base">Choose topic:</span>
         {TAGS.map((tag) => (
