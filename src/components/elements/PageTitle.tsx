9de740624f9ade2978e3ebfca6ce292f0324d32b
ndrvndr <andreavindra37@gmail.com>
2023-10-21 22:18:54 +0700

    style(animation): change direction PageTitle animation

--- src/components/elements/PageTitle.tsx
+++ src/components/elements/PageTitle.tsx
@@ -4,7 +4,7 @@ import BreakLine from "./BreakLine";
 
 export default function PageTitle({ title, description }: PageTitleProps) {
   return (
-    <motion.div initial={{ opacity: 0, y: 50 }} animate={{ opacity: 1, y: 0 }}>
+    <motion.div initial={{ opacity: 0, y: -50 }} animate={{ opacity: 1, y: 0 }}>
       <h1 className="primary text-xl font-bold md:text-2xl">{title}</h1>
       <p className="secondary mt-2 leading-relaxed md:mb-14 md:text-lg lg:mb-0">
         {description}
